5df685356067a7b8dd07b25da8ba37c41e181c30
Jorge Ovalle <jroz9105@gmail.com>
2016-01-30 17:37:23 -0500

    Fonts

--- testSwift/View/testSwiftView.swift
+++ testSwift/View/testSwiftView.swift
@@ -15,6 +15,10 @@ class testSwiftView: UIViewController, testSwiftViewProtocol
     
     
     override func viewDidAppear(animated: Bool) {
+        createUI()
+    }
+    
+    func createUI(){
         let mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: 40.7326808, longitude: -73.9843407), zoomLevel: 12, animated: false)
@@ -30,8 +34,13 @@ class testSwiftView: UIViewController, testSwiftViewProtocol
         a.center = self.view.center
         a.addTarget(self, action: Selector("testEvent:"), forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(a)
-        
     }
+    
+    func createContraints(){
+    
+    }
+    
+    
     
     func testEvent(sender:UIButton){
         presenter?.test()
